e88d658d5abe9126da0dc724792c33100d15fdb4
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-03-25 23:30:54 +0900

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_management/screen/home_screen.dart';
+
+void main() {
+  runApp(const StateManagementApp());
+}
+
+class StateManagementApp extends StatelessWidget {
+  const StateManagementApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
+
