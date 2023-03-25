cc9d2c0d37cf6ffd0efb281fa339bafac13df149
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-03-25 23:33:49 +0900

    Create home_screen.dart

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:state_management/layout/default_layout.dart';
+import 'package:state_management/screen/auto_dipose_modifier_screen.dart';
+import 'package:state_management/screen/family_modifier_screen.dart';
+import 'package:state_management/screen/future_provider_screen.dart';
+import 'package:state_management/screen/listen_provider_screen.dart';
+import 'package:state_management/screen/state_notifier_provider_screen.dart';
+import 'package:state_management/screen/state_provider_screen.dart';
+import 'package:state_management/screen/stream_provider_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  pushProviderScreen(BuildContext context, Widget screen) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => screen,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'HomeScreen',
+      body: ListView(
+        children: [
+          ElevatedButton(
+            onPressed: () => pushProviderScreen(
+              context,
+              StateProviderScreen(),
+            ),
+            child: Text('StateProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () => pushProviderScreen(
+              context,
+              StateNotifierProviderScreen(),
+            ),
+            child: Text('StateNotifierProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () => pushProviderScreen(
+              context,
+              FutureProviderScreen(),
+            ),
+            child: Text('FutureProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () => pushProviderScreen(
+              context,
+              StreamProviderScreen(),
+            ),
+            child: Text('StreamProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () => pushProviderScreen(
+              context,
+              FamilyModifierScreen(),
+            ),
+            child: Text('FamilyModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () => pushProviderScreen(
+              context,
+              AutoDisposeModifierScreen(),
+            ),
+            child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () => pushProviderScreen(
+              context,
+              ListenProviderScreen(),
+            ),
+            child: Text('ListenProviderScreen'),
+          ),
+        ],
+      ),
+    );
+  }
+}
